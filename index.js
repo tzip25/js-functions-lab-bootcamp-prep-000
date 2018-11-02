3e4f174eee9100e557bb2d5fa1cdcd97841f3631
Tali Scheer <tali.scheer@gmail.com>
2018-11-02 20:58:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}`
+  return `It's ${days} days until ${holiday}!`
 }
 
-var holiday
 
